a026319790ebcf9d5b961e09454c8cc633868d03
GANZO9055 <kosta.maslihov@gmail.com>
2023-10-28 14:52:51 +0300

    Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,20 +1,21 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    private int sizeOfEmpty = 10;
-    public String surname;
     public static final String NEW_VALUE = "";
+    public String surname;
     String name;
+
+    private int sizeOfEmpty = 10;
+
+    Broken() { }
+
+    void echo() { }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) { }
 
     void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
-
-    void method(int a, int b, int c, int d, int e, int f, int g) { }
-
-    void echo() { }
-
-    Broken() { }
 }
