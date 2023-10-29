1ab6d478fdeea6cb509cd6b099eb360a38220d9e
GANZO9055 <kosta.maslihov@gmail.com>
2023-10-29 14:06:34 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error errorOne = new Error(true, 1, "error");
+        errorOne.printInfo();
+        Error errorTwo = new Error(false, 2, "error 404");
+        errorTwo.printInfo();
+    }
+}
