c1b35300a6340931979ad3014d6e212baa40bb96
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-03 19:04:52 +0300

    Date. Отображение даты.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -30,5 +34,19 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public static class StartUI {
+        public static void main(String[] args) {
+            Item dateAndTime = new Item();
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+            String dateAndTimeFormat = dateAndTime.getCreated().format(formatter);
+            System.out.println(dateAndTimeFormat);
+        }
     }
 }
