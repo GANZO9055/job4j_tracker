32c60679dc23a07fd5e557fdd29b695663f173bb
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-07 20:41:11 +0300

    toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,5 +9,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String dateAndTimeFormat = dateAndTime.getCreated().format(formatter);
         System.out.println(dateAndTimeFormat);
+        System.out.println(dateAndTime);
     }
 }
