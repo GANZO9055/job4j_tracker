0f2d02ec4b18937e8588f93d84bf34014be719a5
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-07 11:06:32 +0300

    Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,34 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < this.size; i++) {
+            Item item = items[i];
+            if (item != null) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] name = items;
+        Item[] rsl = new Item[name.length];
+        int size = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (key.equals(name[i].getName())) {
+                rsl[size] = name[i];
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        return rsl;
     }
 
     public Item findById(int id) {
