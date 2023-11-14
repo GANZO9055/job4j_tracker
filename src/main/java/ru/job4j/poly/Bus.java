0e570dd78412706b2905e5d7d2c7adaa02797e0b
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-14 22:41:49 +0300

    Интерфейс Input [#181092 #459562]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("Ехать");
     }
 
     @Override
     public void passengers(int numberPassengers) {
-
+        System.out.println("Количество пассажиров: " + numberPassengers);
     }
 
     @Override
     public int priceFuel(int numberFuel) {
-        return 0;
+        return numberFuel * 2;
     }
 }
