b8d2b2bf83828927193ee74fefab76fd40ea52e9
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-18 13:32:24 +0300

    Тесты вывода на консоль [#33585 #460657]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -47,7 +47,7 @@ public class Item {
         return "Item{"
                 + "id = " + id
                 + ", name = '" + name + '\''
-                /*+ ", created = " + created.format(FORMATTER)*/
+                + ", created = " + created.format(FORMATTER)
                 + '}';
     }
 }
