15579af5159e6514a87236b51061403bf4488b4f
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-18 13:47:08 +0300

    Тестирование исключений с junit [#219366 #460722]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -7,12 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FactTest {
     @Test
-    public void whenException() {
+    public void whenExceptionTrue() {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> {
                     new Fact().calc(-3);
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less than 0");
+    }
+
+    @Test
+    public void whenExceptionFalse() {
+        int n = 3;
+        int rsl = new Fact().calc(n);
+        assertThat(rsl).isEqualTo(6);
     }
 }
