7a437ea67ffd063f8216d34718adc26072654cf7
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-22 15:00:49 +0300

    final [#334073 #461631]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int numberEngines = name.equals("A380") ? 4 : COUNT_ENGINE;
+        int numberEngines = "A380".equals(name) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + numberEngines);
     }
 
