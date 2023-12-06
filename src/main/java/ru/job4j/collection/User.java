a8b439ebb8b40a962de7ad0f15225b2eb830a724
GANZO9055 <kosta.maslihov@gmail.com>
2023-12-06 21:04:25 +0300

    Организовать сортировку User [#10034 #464565]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -13,7 +13,8 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name) == 0 ? Integer.compare(age, o.age) : name.compareTo(o.name);
+        int number = name.compareTo(o.name);
+        return number == 0 ? Integer.compare(age, o.age) : number;
     }
 
     @Override
