7c8e50304a542268df9fbb954a8fef5f6998889d
GANZO9055 <kosta.maslihov@gmail.com>
2023-12-09 22:44:30 +0300

    Сортировка номера [#221284 #465179]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -7,8 +7,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        String[] one = left.split(". Task.");
-        String[] two = right.split(". Task.");
+        String[] one = left.split("\\.");
+        String[] two = right.split("\\.");
         return Integer.compare(Integer.parseInt(one[0]), Integer.parseInt(two[0]));
     }
 }
