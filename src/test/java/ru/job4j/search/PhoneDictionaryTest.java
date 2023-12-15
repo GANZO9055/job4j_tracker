df5a141cd57485d94a1ffb474b75eaeb128e9436
GANZO9055 <kosta.maslihov@gmail.com>
2023-12-15 14:24:17 +0300

    Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -34,4 +34,14 @@ class PhoneDictionaryTest {
         ArrayList<Person> person = phones.find("902428");
         assertThat(person).isEmpty();
     }
+
+    @Test
+    public void whenFindByNameInFunction() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
 }
