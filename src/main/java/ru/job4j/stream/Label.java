813d1111f7e047579e1c32e3e8a0396e2dff533a
GANZO9055 <kosta.maslihov@gmail.com>
2023-12-18 14:12:49 +0300

    Перенос методов в Stream API [#279131 #467117]

--- src/main/java/ru/job4j/stream/Label.java
+++ src/main/java/ru/job4j/stream/Label.java
@@ -1,4 +1,5 @@
 package ru.job4j.stream;
+
 import java.util.Objects;
 
 public class Label {
--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,4 +1,5 @@
 package ru.job4j.stream;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
