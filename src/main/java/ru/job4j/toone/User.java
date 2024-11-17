91ae6eea9db5c62a0aa63c7aa5eafb62955b3a96
GANZO9055 <kosta.maslihov@gmail.com>
2024-11-17 23:11:09 +0300

    Update

--- src/main/java/ru/job4j/toone/User.java
+++ src/main/java/ru/job4j/toone/User.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 import lombok.EqualsAndHashCode.Include;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -19,4 +21,7 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "j_user_id")
+    private List<UserMessenger> messengers = new ArrayList<>();
 }
