6955e495d566b4ee16b233f2c7ef8bd1bca839b9
GANZO9055 <kosta.maslihov@gmail.com>
2024-11-17 23:15:16 +0300

    1. Интеграционное тестирование с Hibernate [#6875]

--- src/test/java/ru/job4j/tracker/store/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/store/SqlTrackerTest.java
@@ -69,7 +69,6 @@ public class SqlTrackerTest {
 
     @Test
     public void checkDelete() {
-        
         SqlTracker tracker = new SqlTracker(connection);
         Item item = new Item("item");
         tracker.add(item);
